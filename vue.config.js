0a381161c1e74e5f315f1761fe6bb75b36fdc7a5
tweeddy <alinazimonina@gmail.com>
2024-05-15 12:00:09 +0300

    firebase settings

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,35 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? 'skyro' : '/'}
+module.exports = {
   pwa:{
-    name:"SKYRO PWA",
-    manifestPath: "manifest.json",
     manifestOptions:{
-      
+        capture_links: "existing_client_event",
+        url_handlers:[{
+            origin: "https://harvest-coin.web.app/"
+        }],
         theme_color: "#f3f3f3",
         background_color: "#6891ff",
         icons: [
             {
                 purpose: "maskable",
                 sizes: "512x512",
-                src: "/assets/icon512_maskable.png",
+                src: "img/icons/icon512_maskable.png",
                 type: "image/png"
             },
             {
                 purpose: "any",
                 sizes: "512x512",
-                src: "/assets/icon512_rounded.png",
+                src: "img/icons/icon512_rounded.png",
                 type: "image/png"
             }
         ],
-        orientation: "any",
+        orientation: "portrait",
         display: "standalone",
-        dir: "auto",
-        lang: "en-US",
-        name: "skyro pwa",
+        name: "skyro pwa test",
         short_name: "pwa",
-        start_url: "."
+        start_url: ".",
+        workboxOptions: 'index.html'
     
     }
 }
