34cb050f4bc1bae3bb21ee3f3d74c49bd8369a92
tweeddy <alinazimonina@gmail.com>
2024-05-31 07:45:00 +0300

    firebase init

--- vue.config.js
+++ vue.config.js
@@ -7,26 +7,30 @@ module.exports = {
     },
     name:"field PWA",
     manifestOptions:{
+        capture_links: "existing_client_event",
+        url_handlers:[{
+            origin: "https://harvest-coin.web.app/"
+        }],
         theme_color: "#f3f3f3",
         background_color: "#6891ff",
         icons: [
             {
                 purpose: "maskable",
                 sizes: "512x512",
-                src: 'img/icons/coin.png',
+                src: 'img/icons/icon512_maskable.png',
                 type: "image/png"
             },
             {
                 purpose: "any",
                 sizes: "512x512",
-                src: 'img/icons/coin.png',
+                src: 'img/icons/icon512_rounded.png',
                 type: "image/png"
             }
         ],
         display: "standalone",
         name: "field pwa",
         short_name: "pwa",
-        start_url: "."
+        start_url: ".",
     }
 }
 }
